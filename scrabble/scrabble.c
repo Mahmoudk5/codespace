1707e9146ab127ecd9bd766240bddb9aa9230850
Mahmoudk5 <89376049+Mahmoudk5@users.noreply.github.com>
2023-04-28 12:56:20 +0000

    Fri, Apr 28, 2023, 12:56 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,7 +19,7 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
-    
+
 
 }
 
@@ -36,6 +36,6 @@ int compute_score(string word)
         }
     }
     return points;
-    printf( "this is %i\n", points);
+    
 }
 
